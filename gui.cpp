881b4e45c52aacf18b73509e34a8d23c7236c9e7
Pedro Emanuel Moreira Carvalho <emanuelpedro381@gmail.com>
2026-02-06 17:42:03 -0300

    Criando dinâmica de adicionar e realocar pontos

--- gui.cpp
+++ gui.cpp
@@ -3,38 +3,33 @@
 #include "quadTree.cpp"
 #include <bits/stdc++.h>
 
-int main()
-{
-    
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Boids");
+int main(){
+    const int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 600;
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Boids");
     window.setFramerateLimit(60);
     vector<Point> points;
+    QuadTree quadTree(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, &points); //root
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()){
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left)
-                {
+            if (event.type == sf::Event::MouseButtonPressed){
+                if (event.mouseButton.button == sf::Mouse::Left){
                     int mouseX = event.mouseButton.x;
                     int mouseY = event.mouseButton.y;
 
                     points.emplace_back(static_cast<double>(mouseX), static_cast<double>(mouseY));
+                    quadTree.addPoint(points.size() - 1); //add last point reference to the quadtree
                 }
             }
         }
 
         window.clear(sf::Color::Black);
 
-        for (auto& point : points) {
-            point.render(window);
-        }
+        quadTree.render(window);
 
         window.display();
     }
--- quadTree.cpp
+++ quadTree.cpp
@@ -6,9 +6,12 @@ using namespace std;
 class QuadTree {
     private:
     int x, y, width, height;
-    const int capacity = 10;
+    int points_count = 0;
+    const int capacity = 4;
+    const int LEVEL_LIMIT = 4;
     int level;
     bool divided;
+    vector<Point>* points = nullptr;
     vector<int> points_references;
 
     QuadTree* children[2][2];
@@ -16,21 +19,69 @@ class QuadTree {
 
     public:
 
-    QuadTree(int _x, int _y, int _width, int _height, int _level, QuadTree* _parent = nullptr)
-        : x(_x), y(_y), width(_width), height(_height), level(_level), divided(false), parent(_parent) {
+    QuadTree(int _x, int _y, int _width, int _height, int _level, vector<Point>* _points = nullptr,QuadTree* _parent = nullptr)
+        : x(_x), y(_y), width(_width), height(_height), level(_level), divided(false), parent(_parent), points(_points) {
         children[0][0] = children[0][1] = children[1][0] = children[1][1] = nullptr;
         if (parent){
             this->level = parent->level + 1;
         }else{
             this->level = 0;
         }
+        points_references = vector<int>();
     }
 
     void addPoint(int point_reference) {
-        this->points_references.push_back(point_reference);
+        if(!divided){
+            this->points_references.push_back(point_reference);
+            points_count++;
+            if(points_count > capacity and level < LEVEL_LIMIT){
+                this->subdivide();
+                this->realocatePoints();
+            }
+        }else{
+            this->alocatePoint(point_reference);
+        }
     }
 
-    void render(sf::RenderWindow& window, vector<Point>& points) {
+    void subdivide() {
+        if (divided) return;
+
+        int halfWidth = width / 2;
+        int halfHeight = height / 2;
+
+        children[0][0] = new QuadTree(x, y, halfWidth, halfHeight, level + 1, points, this);
+        children[0][1] = new QuadTree(x + halfWidth, y, halfWidth, halfHeight, level + 1, points, this);
+        children[1][0] = new QuadTree(x, y + halfHeight, halfWidth, halfHeight, level + 1, points, this);
+        children[1][1] = new QuadTree(x + halfWidth, y + halfHeight, halfWidth, halfHeight, level + 1, points, this);
+
+        divided = true;
+    }
+
+    void alocatePoint(int point_reference) {
+        Point& p = this->points->at(point_reference);
+        double px = p.getX();
+        double py = p.getY();
+
+        int indexX = (px >= x + width / 2) ? 1 : 0;
+        int indexY = (py >= y + height / 2) ? 1 : 0;
+
+        if (children[indexY][indexX]) {
+            children[indexY][indexX]->addPoint(point_reference);
+            cout << "Level<<" << level<< " Children:" << indexX << " " << indexY << endl;
+        }
+    }
+
+    void realocatePoints() {
+        vector<int> tempPoints = points_references;
+        points_references.clear();
+        points_count = 0;
+
+        for (int ref : tempPoints) {
+            this->alocatePoint(ref);
+        }
+    }
+
+    void render(sf::RenderWindow& window) {
         sf::RectangleShape rectangle(sf::Vector2f(static_cast<float>(width), static_cast<float>(height)));
         rectangle.setPosition(static_cast<float>(x), static_cast<float>(y));
         rectangle.setFillColor(sf::Color::Transparent);
@@ -38,17 +89,17 @@ class QuadTree {
         rectangle.setOutlineThickness(1.0f);
         window.draw(rectangle);
 
-        for (int ref : points_references) {
-            points[ref].render(window);
-        }
-
         if (divided) {
             for (int i = 0; i < 2; ++i) {
                 for (int j = 0; j < 2; ++j) {
                     if (children[i][j]) {
-                        children[i][j]->render(window, points);
+                        children[i][j]->render(window);
                     }
                 }
+            }
+        } else {
+            for (int ref : points_references) {
+                this->points->at(ref).render(window);
             }
         }
     }
